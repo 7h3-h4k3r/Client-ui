779a126db1a4a5c1a0c4fcb373d77fb45ac8bdc1
sridharanitharan <username420sri@gmail.com>
2026-03-06 07:18:10 +0530

    client Message BroadCasting

---
+++ server/chatservice.py
@@ -0,0 +1,48 @@
+import socket
+from threading import Thread
+clients_list = []
+class GroupChat:
+    
+    def __init__(self,host='0.0.0.0',port=5656):
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.host = host 
+        self.port = port 
+        
+    def clients(self,conn,addr):
+        clients_list.append(conn)
+        with conn: 
+            while True:
+                try:
+                    data = conn.recv(1024)
+                    if not data:
+                        break 
+                    self.broadcast(data,conn)
+                except Exception as e:
+                    print(f'Error Client Connection {addr[0]} : Error {e}\r\n')
+                    break 
+        self.broadcast(f'Client Disconnect {addr[0]}\r\n'.encode())
+        clients_list.pop(conn)
+    
+    def broadcast(self,data,sender):
+        msg = data.decode()
+        for conn in clients_list:
+            if conn!=sender:
+                try: 
+                    conn.sendall(msg.encode())
+                except:
+                    clients_list.pop(conn)
+            
+                
+    def setConf(self):
+        self.socket.bind((self.host,self.port))
+        self.socket.listen()
+        
+    def getConn(self):
+        self.setConf()
+        while True:
+            conn , addr = self.socket.accept()
+            t = Thread(target=self.clients,args=(conn,addr))
+            t.start()
+        
+GroupChat().getConn()
+    
